4ba5fb15bca0c0221aefc70ea3cce92262100f0c
Zach Selindh <zachselindh@gmail.com>
2019-06-27 19:40:01 -0500

    Made create page stateful, added methods.

--- client/src/components/CreatePage/index.js
+++ client/src/components/CreatePage/index.js
@@ -1,16 +1,55 @@
 import React, { Component } from "react";
+import API from "../../utils/API";
 import "./style.css";
 
 class CreatePage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      description: ""
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    API.saveTodo({
+      title: this.state.title,
+      description: this.state.description
+    })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div id="display-area-z" className="col-12">
             <h1>Enter your Todo:</h1>
-            <form action="/api/todos" method="POST">
-              <input type="text" placeholder="Title" name="title" />
-              <input type="text" placeholder="description" name="description" />
+            <form
+              onSubmit={this.handleSubmit}
+              action="/api/todos"
+              method="POST"
+            >
+              <input
+                type="text"
+                placeholder="Title"
+                name="title"
+                onChange={this.handleInputChange}
+              />
+              <input
+                type="text"
+                placeholder="Description"
+                name="description"
+                onChange={this.handleInputChange}
+              />
               <button type="submit">Submit</button>
             </form>
           </div>
