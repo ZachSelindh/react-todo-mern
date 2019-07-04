edf1fca250f5a876e9d707cc61ca31fc14ae8f75
Zach Selindh <zachselindh@gmail.com>
2019-07-03 20:27:27 -0500

    Styled header nav links.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -7,17 +7,16 @@ class Header extends Component {
       <div className="row todo-header">
         <div className="col-12">
           <div className="row">
-            <div className="col-sm-6 col-md-4">
+            <div className="col-sm-6 col-md-5">
               <h1>MERN-stack Todo List</h1>
             </div>
             <div className="col-sm-6 col-md-3">
               <p>A full-stack todo app</p>
             </div>
-            <div className="col-sm-12 col-md-5">
-              <div className="row">
+            <div className="col-sm-12 col-md-4">
+              <div id="nav-header-z" className="row">
                 <a href="/">Home</a>
                 <a href="/completed">Completed</a>
-                <a href="/create"> Create</a>
               </div>
             </div>
           </div>
