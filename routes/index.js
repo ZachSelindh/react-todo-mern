07c63aa5d958e6c53a608f722e46d43e2fb0178c
Zach Selindh <zachselindh@gmail.com>
2019-07-16 18:34:43 -0500

    Added route for build.

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,12 @@ router.use("/users", userRoutes);
 
 // Send React start point if no routes are called.
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  res.sendFile(
+    path.join(
+      (__dirname, "../client/build/index.html") ||
+        (__dirname, "../client/public/index.html")
+    )
+  );
 });
 
 module.exports = router;
