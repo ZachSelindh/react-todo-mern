0dbe57a86a529e6e456c0e9207f4f9d63edfc3fb
Zach Selindh <zachselindh@gmail.com>
2019-07-16 18:48:58 -0500

    Changed static route for build.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "client/public")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 const routes = require("./routes");
 
