06c075554b6213cae805f76857da34c62eb05465
Zach Selindh <zachselindh@gmail.com>
2019-07-16 19:07:32 -0500

    Set up .env for start route.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const userRoutes = require("./user");
 const tokenRoutes = require("./token");
+require("dotenv").config();
 
 // API Routes
 router.use("/api/todos", apiRoutes);
@@ -15,10 +16,13 @@ router.use("/users", userRoutes);
 
 // Send React start point if no routes are called.
 router.use(function(req, res) {
-  /* // Dev route
-  res.sendFile(path.join(__dirname, "../client/public/index.html")); */
-  // Build route
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  if (process.env.ENVIRONMENT === "development") {
+    // Dev route
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  } else if (process.env.ENVIRONMENT === "production") {
+    // Build route
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  }
 });
 
 module.exports = router;
